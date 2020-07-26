0eb2554146952a4495434dc2e56db9b87812e00e
ilya <beqasmile@gmail.com>
2020-07-26 19:03:37 +0300

    add drivers dal

--- DBConnection/CarBL/DriversDal.cs
+++ DBConnection/CarBL/DriversDal.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using CarCommon.Properties;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,72 @@ namespace CarBL
 {
     class DriversDal
     {
+        public List<Driver> SelectDrivers()
+        {
+            using (SqlCommand command = new SqlCommand("SELECT * FROM Driver ", CarConnection.GetInstance().SqlConnection))
+            {
+                //
+                // Add new SqlParameter to the command.
+                //
+                //command.Parameters.Add(new SqlParameter("Name", dogName));
+                //
+                // Read in the SELECT results.
+                //
+                List<Driver> driverList = new List<Driver>();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Driver newDriver = new Driver(reader.GetInt32(0), reader.GetString(2), reader.GetInt32(1), reader.GetString(3), reader.GetInt32(4));
+
+                    driverList.Add(newDriver);
+                }
+                return new List<Driver>();
+            }
+        }
+
+        public void InsertDriver(Driver driver)
+        {
+            SqlCommand sqlCommand = new SqlCommand(@"INSERT INTO Drivers VALUES @ID, @DriverName, @Age, @Address, @DriversLessonType", CarConnection.GetInstance().SqlConnection);
+
+
+            SqlParameter sqlParameter = new SqlParameter("ID", driver.Id);
+            sqlCommand.Parameters.Add(sqlParameter);
+
+            SqlParameter sqlParameter1 = new SqlParameter("DriverName", driver.DriverName);
+            sqlCommand.Parameters.Add(sqlParameter1);
+            SqlParameter sqlParameter2 = new SqlParameter("Age", driver.Age);
+            sqlCommand.Parameters.Add(sqlParameter2);
+            SqlParameter sqlParameter3 = new SqlParameter("Adress", driver.Address);
+            sqlCommand.Parameters.Add(sqlParameter3);
+            SqlParameter sqlParameter4 = new SqlParameter("DriversLessonType", driver.DriversLessonType);
+            sqlCommand.Parameters.Add(sqlParameter4);
+
+            sqlCommand.ExecuteScalar();
+        }
+
+        public void UpdateDriver(Driver driver)
+        {
+            SqlCommand sqlCommand = new SqlCommand(@"Update Drivers set DriverName = @DriverName, Age=@Age, Adress= @Adress, DriversLessonType=@DriversLessonType where Id = @Id", CarConnection.GetInstance().SqlConnection);
+
+            SqlParameter sqlParameter = new SqlParameter("ID", driver.Id);
+            sqlCommand.Parameters.Add(sqlParameter);
+
+            SqlParameter sqlParameter1 = new SqlParameter("DriverName", driver.DriverName);
+            sqlCommand.Parameters.Add(sqlParameter1);
+            SqlParameter sqlParameter2 = new SqlParameter("Age", driver.Age);
+            sqlCommand.Parameters.Add(sqlParameter2);
+            SqlParameter sqlParameter3 = new SqlParameter("Adress", driver.Address);
+            sqlCommand.Parameters.Add(sqlParameter3);
+            SqlParameter sqlParameter4 = new SqlParameter("DriversLessonType", driver.DriversLessonType);
+            sqlCommand.Parameters.Add(sqlParameter4);
+
+        }
+
+        public void DeleteDriver(Driver driver)
+        {
+            SqlCommand sqlCommand = new SqlCommand(@"Delete * from Drivers where Id = @Id", CarConnection.GetInstance().SqlConnection);
+            SqlParameter sqlParameter = new SqlParameter("ID", driver.Id);
+            sqlCommand.Parameters.Add(sqlParameter);
+        }
     }
 }
---
+++ DBConnection/CarCommon/Driver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarCommon.Properties
+{
+    public class Driver
+    {
+        private int id;
+        private string driverName;
+        private int age;
+        private string address;
+        private int driversLessonType;
+
+        public int Id { get => id; set => id = value; }
+        public string DriverName { get => driverName; set => driverName = value; }
+        public int Age { get => age; set => age = value; }
+        public string Address { get => address; set => address = value; }
+        public int DriversLessonType { get => driversLessonType; set => driversLessonType = value; }
+
+
+        public Driver(int _Id, string _DriverName, int _Age, string _Address, int _DriversLessonType)
+        {
+            this.id = _Id;
+            this.driverName = _DriverName;
+            this.age = _Age;
+            this.address = _Address;
+            this.driversLessonType = _DriversLessonType;
+        }
+
+    }
+}
